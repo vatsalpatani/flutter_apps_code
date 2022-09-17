24201525a80178d2f298576c5dcad72570b398b3
vatsal patani <vatsalpatani@gmail.com>
2022-09-17 14:22:51 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,183 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My App',
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color.fromARGB(255, 19, 25, 47),
+        primarySwatch: Colors.blue,
+      ),
+      home: //Home(),
+          const MyHomePage(title: 'Counting App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  var title = '';
+  String text = "No Value Entered";
+
+  void _setText() {
+    setState(() {
+      _counter = int.parse(title);
+    });
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  void _reset() {
+    setState(() {
+      _counter = 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            const SizedBox(
+              height: 50,
+            ),
+            SizedBox(
+              width: 300,
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  enabledBorder: const OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white, width: 0.0),
+                  ),
+                  labelText: 'Enter a Number',
+                  labelStyle: const TextStyle(color: Colors.white),
+                  hintText: 'Enter a Number here',
+                  hintStyle:
+                      const TextStyle(fontSize: 20.0, color: Colors.white),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide:
+                        const BorderSide(color: Colors.white, width: 2.0),
+                    borderRadius: BorderRadius.circular(25.0),
+                  ),
+                ),
+                style: const TextStyle(color: Colors.white),
+                keyboardType: TextInputType.number,
+                onChanged: (value) => title = value,
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Align(
+              child: ElevatedButton.icon(
+                onPressed: _setText,
+                icon: const Icon(
+                  Icons.send,
+                ),
+                label: const Text('Submit'),
+              ),
+            ),
+            const SizedBox(
+              height: 80,
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Text(
+                'value = '
+                '$_counter',
+                style: TextStyle(
+                  fontSize: 40,
+                  foreground: Paint()
+                    ..style = PaintingStyle.stroke
+                    ..strokeWidth = 2
+                    ..color = Colors.blue[700]!,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Stack(
+        fit: StackFit.expand,
+        children: [
+          Positioned(
+            left: 30,
+            bottom: 20,
+            child: FloatingActionButton(
+              backgroundColor: Colors.white,
+              //splashColor: Color.fromARGB(255, 255, 0, 0),
+              foregroundColor: Colors.black,
+              onPressed: _incrementCounter,
+              tooltip: 'Increment',
+              child: const Icon(Icons.add),
+            ),
+          ),
+          Positioned(
+            bottom: 20,
+            right: 30,
+            child: FloatingActionButton(
+              backgroundColor: Colors.white,
+              //splashColor: Color.fromARGB(255, 255, 0, 0),
+              foregroundColor: Colors.black,
+              onPressed: _decrementCounter,
+              tooltip: 'Decrement',
+              child: const Icon(Icons.remove),
+            ),
+          ),
+          Positioned(
+            left: 90,
+            bottom: 20,
+            right: 90,
+            child: FloatingActionButton(
+              backgroundColor: Colors.white,
+              foregroundColor: Colors.black,
+              onPressed: _reset,
+              tooltip: 'Reset',
+              child: const Icon(Icons.exposure_zero_sharp),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
